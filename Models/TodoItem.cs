7df9e4df2685463e5e7f272a94432bd0596cb603
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-09-23 19:28:06 +0200

    added todoitem model

---
+++ Models/TodoItem.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApplication_MVC.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public string Priority { get; set; }
+
+        [Required]
+        public bool isDone { get; set; }
+    }
+}
