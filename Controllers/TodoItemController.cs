f593ba06dc689c2790f990d56ce047063dbe0230
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-09-23 20:02:51 +0200

    ienumerable type

--- Controllers/TodoItemController.cs
+++ Controllers/TodoItemController.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ToDoApplication_MVC.Data;
+using ToDoApplication_MVC.Models;
 
 namespace ToDoApplication_MVC.Controllers
 {
     public class TodoItemController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public TodoItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            IEnumerable<TodoItem> objTodoItemList = _db.TodoItems;
+
             return View();
         }
     }
