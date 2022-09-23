bef7525695b06c363954dbfbc98e4579fe7cd08b
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-09-23 19:42:07 +0200

    app db context

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoApplication_MVC.Models;
+
+namespace ToDoApplication_MVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
