1f038267cedec21f48df72cc6f63058d7694bbc7
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-09-23 19:54:38 +0200

    todo item controller

---
+++ Controllers/TodoItemController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApplication_MVC.Controllers
+{
+    public class TodoItemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
